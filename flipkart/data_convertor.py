3675a42ce763e1cf58b6c86942584366b9cfdcc8
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-02-06 17:44:36 -0500

    Added data convertor takes df as input and converts into documents

--- flipkart/data_convertor.py
+++ flipkart/data_convertor.py
@@ -1,3 +1,18 @@
 import pandas as pd
 from langchain_core.documents import Document
 
+class Dataconvertor:
+    def __init__(self, file_path:str):
+        self.file_path= file_path
+
+    def convert(self):
+        df= pd.read_csv(self.file_path)[['product_title','review']]
+        docs =[ Document(page_content= rows['review'],
+                meta_data= {"product_name": rows['product_title']})
+             for _, rows in df.iterrows()
+              ]
+        
+        return docs
+
+    
+
